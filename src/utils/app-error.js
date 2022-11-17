e88d86b90af22c872f6dca43b5f10001a893b996
Samuel Kirigha <dkirigha18@gmail.com>
2022-11-17 17:26:29 +0300

    added error handlers

---
+++ src/utils/app-error.js
@@ -0,0 +1,71 @@
+const STATUS_CODES = {
+  Ok: 200,
+  BAD_REQUEST: 400,
+  UN_AUTHORIZED: 403,
+  NOT_FOUND: 404,
+  INTERNAL_ERROR: 500,
+};
+
+class AppError extends Error {
+  constructor(
+    name,
+    statusCode,
+    description,
+    isOperational,
+    errorStack,
+    loginErrorResponse
+  ) {
+    super(description);
+    Object.setPrototypeOf(this, new.target.prototype);
+    this.name = name;
+    this.statusCode = statusCode;
+    (this.isOperational = isOperational),
+      (this.errorStack = errorStack),
+      (this.logError = loginErrorResponse),
+      Error.captureStackTrace(this);
+  }
+}
+
+//api Specific errors
+class APIError extends APIError {
+  constructor(
+    name,
+    statusCode = STATUS_CODES.INTERNAL_ERROR,
+    description = "Internal server Error",
+    isOperational = true
+  ) {
+    super(name, statusCode, description, isOperational);
+  }
+}
+
+//400
+class BadRequestError extends AppError {
+  constructor(description = "Bad request", loginErrorResponse) {
+    super(
+      "NOT FOUND",
+      STATUS_CODES.BAD_REQUEST,
+      description,
+      true,
+      false,
+      loginErrorResponse
+    );
+  }
+}
+
+//400
+class ValidationError extends AppError {
+  constructor(description = "Validation Error", errorStack) {
+    super("BAD REQUEST", STATUS_CODES.BAD_REQUEST),
+      description,
+      true,
+      errorStack;
+  }
+}
+
+module.exports = {
+  AppError,
+  APIError,
+  BadRequestError,
+  ValidationError,
+  STATUS_CODES,
+};
